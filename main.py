efbbc01b66b3f94d690a67ef6fb47eb0205faa0c
ralphgehy <rgehy84@gmail.com>
2022-12-15 22:47:28 -0500

    update main file for env variables test

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("BLOG_APP_SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("BLOG_APP_DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("BLOG_APP_DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
